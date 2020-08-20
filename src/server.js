fb07a71a466714079277186dc05909ca1fdac43a
fredluetkemeier@gmail.com <fredluetkemeier@gmail.com>
2020-08-19 23:20:11 -0500

    Added express server code

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const PORT = 4000;
+
+const app = express();
+
+const schema = require('./schema');
+
+app.post(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: false,
+    })
+);
+
+app.get(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
