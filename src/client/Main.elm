7cfb20aaa324cc56178598ef0fe41293e443ffc8
Fred Luetkemeier <fluetkemeier@midwayusa.com>
2020-08-21 15:43:38 -0500

    Start of app

--- src/client/Main.elm
+++ src/client/Main.elm
@@ -1,14 +1,52 @@
 module Main exposing (main)
 
 import Browser
+import Element exposing (Element, el, none)
+import Html exposing (Html)
 
-main : Program () Cmd msg 
-main = 
-    Browser.element 
-    {
-        init = init,
-        view = view
-        update = update,
-        subscriptions = \_ -> Sub.none
-    }
-    
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    Element.layout [] none
