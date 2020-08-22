a27256835135beae7b3a20c0762c5377130ccb92
fredluetkemeier@gmail.com <fredluetkemeier@gmail.com>
2020-08-22 01:55:46 -0500

    Added quicksand font

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import { Elm } from './Main';
+import './styles/font.scss';
 
 const app = Elm.Main.init({
     node: document.getElementById('app'),
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/client',
@@ -7,10 +8,28 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/dist/',
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(woff|ttf|otf|eot|woff2|svg)$/i,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[contenthash].[ext]',
+                },
+            },
+        ],
+    },
     resolve: {
         extensions: ['.js', '.elm'],
     },
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             title: 'mpm',
             inject: 'body',
