0883971fd79ed5b7da24fa2e4e03ea00844c6558
fredluetkemeier@gmail.com <fredluetkemeier@gmail.com>
2020-09-26 02:26:10 -0500

    Enabled asset cache

--- src/main/server.js
+++ src/main/server.js
@@ -4,7 +4,13 @@ const path = require('path');
 function startServer(port) {
     const app = express();
 
-    app.use('/assets', express.static(path.join(__dirname, '../../assets')));
+    app.use(
+        '/assets',
+        express.static(path.join(__dirname, '../../assets'), {
+            cacheControl: true,
+            maxAge: 24 * 60 * 1000,
+        })
+    );
     app.use('/dist', express.static(path.join(__dirname, '../../dist')));
 
     app.get('/*', (req, res) => {
